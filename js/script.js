188fc62c6645080dcbbaf82a36bd91ccfaa95204
Michael Provencher <michaelprovencher15@gmail.com>
2019-06-24 10:20:01 -0400

    show images based on radio buttons & eslinting

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const chords = ['A', 'Em', 'C', 'G', 'D', 'Am', 'E', 'F'];
 const chords7th = ['A7', 'B7', 'C7', 'G7', 'D7', 'E7', 'Fmaj7'];
-const { notes } = localStorage;
+const { notes, showImages } = localStorage;
 const timer = Number(localStorage['timer']);
 
 let chordPlaying;
@@ -8,45 +8,53 @@ let chordPlaying;
 let timerCounter = timer;
 document.querySelector('#timer').innerHTML = timerCounter;
 
+if (showImages === 'true') {
+    const imgTags = document.querySelectorAll('.img');
+    imgTags.forEach(function(imgTag) {
+        imgTag.classList.remove('hidden');
+    });
+}
+
 if (notes === 'normal') {
-  init(chords);
+    init(chords);
 } else if (notes === '7th') {
-  init(chords7th);
+    init(chords7th);
 }
 
 function init(chordsToPlay) {
-  chordPlaying = getRandomChord(chordsToPlay);
-  showChords(chordsToPlay);
-  setInterval(function() {
+    chordPlaying = getRandomChord(chordsToPlay);
     showChords(chordsToPlay);
-  }, timer * 1000);
+    setInterval(function() {
+        showChords(chordsToPlay);
+    }, timer * 1000);
 }
 
 //Rechercher un nouvel accord
 function getRandomChord(arrayChords) {
-  return arrayChords[Math.floor(Math.random() * arrayChords.length)];
+    return arrayChords[Math.floor(Math.random() * arrayChords.length)];
 }
 
 function showChords(chords) {
-  let nextChordToBePlayed = getRandomChord(chords);
-  showChord('.chord', chordPlaying);
-  showChord('.next-chord', nextChordToBePlayed);
-  chordPlaying = nextChordToBePlayed;
+    let nextChordToBePlayed = getRandomChord(chords);
+    showChord('.chord', chordPlaying);
+    showChord('.next-chord', nextChordToBePlayed);
+    chordPlaying = nextChordToBePlayed;
 }
 
 function showChord(selector, chord) {
-  document.querySelector(selector).innerHTML = chord;
-
-  document
-    .querySelector(`${selector}-img`)
-    .setAttribute('src', `img/${chord}.gif`);
+    document.querySelector(selector).innerHTML = chord;
+    if (showImages === 'true') {
+        document
+            .querySelector(`${selector}-img`)
+            .setAttribute('src', `img/${chord}.gif`);
+    }
 }
 
 //Change d'accord aux 4 secondes
 setInterval(function() {
-  timerCounter--;
-  document.querySelector('#timer').innerHTML = timerCounter;
-  if (timerCounter === 1) {
-    timerCounter = timer + 1;
-  }
+    timerCounter--;
+    document.querySelector('#timer').innerHTML = timerCounter;
+    if (timerCounter === 1) {
+        timerCounter = timer + 1;
+    }
 }, 1000);
