f37dd8d9916772fc58837ec41cf5bc9411f205e5
Michael Provencher <michaelprovencher15@gmail.com>
2020-04-11 15:33:57 -0400

    added other exerices

--- src/components/ExercisesLinks.tsx
+++ src/components/ExercisesLinks.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function ExercisesLinks() {
-    return <Link to="/exercise/spider">Spider</Link>;
+    return (
+        <>
+            <Link to="/exercise/spider">Spider</Link>
+            <Link to="/exercise/finger-gym">Finger Gym</Link>
+            <Link to="/exercise/minimum-movement">Minimum movement</Link>
+        </>
+    );
 }
 
 export default ExercisesLinks;
