1875683fac472a8ce7c11563d0b6f8d790de7619
Michael Provencher <michaelprovencher15@gmail.com>
2020-11-30 21:14:07 -0500

    added tests for Chord component

---
+++ src/components/__tests__/Chord.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Chord from '../Chord';
+
+test('should render', () => {
+    render(<Chord chord="A" displayImage selected={false} />);
+    expect(screen.getByText('A')).toBeInTheDocument();
+    expect(screen.getByAltText('A')).not.toHaveClass('selected-chord');
+});
+
+test('should render', () => {
+    render(<Chord chord="A" displayImage selected />);
+    expect(screen.getByAltText('A')).toHaveClass('selected-chord');
+});
+
+test('should render', () => {
+    render(<Chord chord="A" displayImage={false} selected />);
+    expect(screen.queryByAltText('A')).not.toBeInTheDocument();
+});
